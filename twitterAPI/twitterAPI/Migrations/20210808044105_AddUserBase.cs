737d4ab6b43c8c300f81a6681a809108c9818282
Dante Ruiz <dante.t.ruiz.iii@accenture.com>
2021-08-09 20:29:41 +0800

    user feature update

---
+++ twitterAPI/twitterAPI/Migrations/20210808044105_AddUserBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace twitterAPI.Migrations
+{
+    public partial class AddUserBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Twitter",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "UserID", "DateCreated", "DateModified", "Email", "Name", "Password" },
+                values: new object[] { -1, new DateTime(2021, 8, 8, 4, 41, 5, 191, DateTimeKind.Utc).AddTicks(7061), new DateTime(2021, 8, 8, 4, 41, 5, 191, DateTimeKind.Utc).AddTicks(7315), "admin@email.com", "admin", "admin123" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Twitter_UserID",
+                table: "Twitter",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Twitter_User_UserID",
+                table: "Twitter",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Twitter_User_UserID",
+                table: "Twitter");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Twitter_UserID",
+                table: "Twitter");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Twitter");
+        }
+    }
+}
---
+++ twitterAPI/twitterAPI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace twitterAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public virtual ICollection<Tweet> Tweets { get; set; }
+    }
+}
